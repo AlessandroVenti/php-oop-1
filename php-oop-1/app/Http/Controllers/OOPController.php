c16dd22e0f3006082079ec45b8bf44961828e57a
Alessandro Venti <alessandro.venti@icloud.com>
2021-05-19 14:14:56 +0200

    Setup new laravel project

---
+++ php-oop-1/app/Http/Controllers/OOPController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OOPController extends Controller
+{
+    public function oopFunction() {
+
+        return view('pages.oopExercise');
+    }
+}
---
+++ php-oop-1/resources/views/pages/oopExercise.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+     <main>
+          <h1>I'm the main</h1>
+     </main>
+    
+@endsection
